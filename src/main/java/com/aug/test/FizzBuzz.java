408e4f415ae99d90b1fd9e2d73f30d53dd55f334
adisorn <armchockaumnuai@gmail.com>
2014-11-11 11:35:06 +0700

    refactor production code

--- src/main/java/com/aug/test/FizzBuzz.java
+++ src/main/java/com/aug/test/FizzBuzz.java
@@ -2,15 +2,15 @@ package com.aug.test;
 
 public class FizzBuzz {
 
-    public String getMessage(String datainput) {
-        if (Integer.parseInt(datainput) % 15 == 0) {
+    public String getMessage(String input) {
+        if (Integer.parseInt(input) % 15 == 0) {
             return "fizzbuzz";
-        }else if (Integer.parseInt(datainput) % 3 == 0) {
+        }else if (Integer.parseInt(input) % 3 == 0) {
             return "fizz";
-        } else if (Integer.parseInt(datainput) % 5 == 0) {
+        } else if (Integer.parseInt(input) % 5 == 0) {
             return "buzz";
         } else {
-            return datainput;
+            return input;
         }
     }
 }
