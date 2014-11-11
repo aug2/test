f087cf134a21798424c7a8ead5737559b5d20296
adisorn <armchockaumnuai@gmail.com>
2014-11-11 11:33:29 +0700

    before step in unit test

--- src/test/java/com/aug/test/FizzBuzzTest.java
+++ src/test/java/com/aug/test/FizzBuzzTest.java
@@ -1,37 +1,42 @@
 package com.aug.test;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp(){
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void input1ShouldReturn1(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals("1", fizzBuzz.getMessage("1"));
+        assertEquals("1", fizzBuzz.getMessage("1"));
     }
     
     @Test
     public void input3ShouldReturnfizz(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals("fizz", fizzBuzz.getMessage("3"));
+        assertEquals("fizz", fizzBuzz.getMessage("3"));
     }
     
     @Test
     public void input5ShouldReturnbuzz(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals("buzz", fizzBuzz.getMessage("5"));
+        assertEquals("buzz", fizzBuzz.getMessage("5"));
     }
     
     
     @Test
     public void input15ShouldReturnfizzbuzz(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals("fizzbuzz", fizzBuzz.getMessage("15"));
+        assertEquals("fizzbuzz", fizzBuzz.getMessage("15"));
     }
     
     @Test
     public void input2ShouldReturn2(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        Assert.assertEquals("2", fizzBuzz.getMessage("2"));
+        assertEquals("2", fizzBuzz.getMessage("2"));
     }
 }
