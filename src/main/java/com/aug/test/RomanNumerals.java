85f01601485b141a0cdcbe885bf14a9e6f45d54b
kik <paopeed.kik@gmail.com>
2014-12-08 16:00:54 +0700

    RomanEdit2

--- src/main/java/com/aug/test/RomanNumerals.java
+++ src/main/java/com/aug/test/RomanNumerals.java
@@ -1,7 +1,7 @@
 package com.aug.test;
 
 public class RomanNumerals {
-<<<<<<< HEAD
+
     public String convert(int input) {
     	if(input==1){
     		return "I";
@@ -12,18 +12,5 @@ public class RomanNumerals {
     		return null;
     }
     
-=======
-	public String convert(int input) {
 
-		if ((input) == 1) {
-			return "I";
-		} else if ((input) == 2) {
-			return "II";
-
-		} else {
-			return null;
-		}
-
-	}
->>>>>>> aa8653b963d961b925889cc612f2fffdbd65cb88
 }
