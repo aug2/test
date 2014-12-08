495c56e4265c1d13cd98edd69060c0faed28d008
adisorn <armchockaumnuai@gmail.com>
2014-12-08 15:30:12 +0700

    remove unused code in NumberInWords

--- src/main/java/com/aug/test/NumberInWords.java
+++ src/main/java/com/aug/test/NumberInWords.java
@@ -4,13 +4,6 @@ public class NumberInWords {
     public String convert(int input) {
     	String words[] = {"Zero","One","Two","Three","Four"};
     	
-    	if(input > 4) {
-    		switch (input) {
-			case 5: return "Four";
-			default: return "Five";
-			}
-    	}else return words[input];
-    		
-    	
+    	return words[input];
     }
 }
