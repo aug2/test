75e3b4c32bd9f41cfaabab34b1d7f22bc5e847ac
bill <bisbifltl@gmail.com>
2014-12-08 15:54:47 +0700

    E'Bill hewkluay

--- src/main/java/com/aug/test/RomanNumerals.java
+++ src/main/java/com/aug/test/RomanNumerals.java
@@ -2,6 +2,13 @@ package com.aug.test;
 
 public class RomanNumerals {
     public String convert(int input) {
-        return "I";
+    	if(input==1){
+    		return "I";
+    	}
+    	else if(input==2)
+        return "II";
+    	else
+    		return null;
     }
+    
 }
