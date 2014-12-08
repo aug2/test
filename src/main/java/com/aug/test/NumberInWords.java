7d1b1d22acd6be05c446312d61c83d2570ebe6dc
bill <bisbifltl@gmail.com>
2014-12-08 15:16:00 +0700

    NumberInWords

--- src/main/java/com/aug/test/NumberInWords.java
+++ src/main/java/com/aug/test/NumberInWords.java
@@ -1,7 +1,16 @@
 package com.aug.test;
 
 public class NumberInWords {
-    public String convert(int number) {
-        return "Zero";
+    public String convert(int input) {
+    	String words[] = {"Zero","One","Two","Three","Four"};
+    	
+    	if(input > 4) {
+    		switch (input) {
+			case 5: return "Four";
+			default: return "Five";
+			}
+    	}else return words[input];
+    		
+    	
     }
 }
